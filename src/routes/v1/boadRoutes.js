eb09809ab793fb3601336b17cf1f698d7d2936c3
dhquan <danghongquan198@gmail.com>
2025-08-18 06:52:45 +0700

    api: setup api

--- src/routes/v1/boadRoutes.js
+++ src/routes/v1/boadRoutes.js
@@ -3,20 +3,16 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
-
 import express from 'express'
 import { StatusCodes } from 'http-status-codes'
-import { boardRoutes } from './boadRoutes.js'
 
 const Router = express.Router()
 
-/* APIs v1/status */
-Router.get('/status', (req, res) => {
-  res.status(StatusCodes.OK).json({ status: 'OK' })
-})
-
-/* Boards api */
-Router.use('/boards', boardRoutes)
-
-export const APIs_V1 = Router
-export default Router
+Router.route('/')
+  .get((req, res) => {
+    res.status(StatusCodes.OK).json({ message: 'Get all boards' })
+  })
+  .post((req, res) => {
+    res.status(StatusCodes.CREATED).json({ message: 'Create a new board' })
+  })
+export const boardRoutes = Router
--- src/server.js
+++ src/server.js
@@ -8,29 +8,32 @@ import express from 'express'
 import { mapOrder } from '~/utils/sorts.js'
 import connectDB from '~/config/mongodb.js'
 import env from '~/config/environment.js'
+import { APIs_V1 } from '~/routes/v1/index.js'
 
 const app = express()
 
 // Connect to MongoDB
 connectDB()
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(
-    mapOrder(
-      [
-        { id: 'id-1', name: 'One' },
-        { id: 'id-2', name: 'Two' },
-        { id: 'id-3', name: 'Three' },
-        { id: 'id-4', name: 'Four' },
-        { id: 'id-5', name: 'Five' },
-      ],
-      ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-      'id'
-    )
-  )
-  res.end('<h1>Hello World!</h1><hr>')
-})
+app.use('/v1', APIs_V1)
+
+// app.get('/', (req, res) => {
+//   // Test Absolute import mapOrder
+//   console.log(
+//     mapOrder(
+//       [
+//         { id: 'id-1', name: 'One' },
+//         { id: 'id-2', name: 'Two' },
+//         { id: 'id-3', name: 'Three' },
+//         { id: 'id-4', name: 'Four' },
+//         { id: 'id-5', name: 'Five' },
+//       ],
+//       ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+//       'id'
+//     )
+//   )
+//   res.end('<h1>Hello World!</h1><hr>')
+// })
 
 app.listen(env.APP_PORT, env.APP_HOST, () => {
   // eslint-disable-next-line no-console
