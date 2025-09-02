f62954353688d1b4dcebf4f13def35a99d3f6574
dhquan <danghongquan198@gmail.com>
2025-09-02 14:24:46 +0700

    create colums, cards

--- src/services/cardService.js
+++ src/services/cardService.js
@@ -1,10 +1,15 @@
 import { cardModel } from '~/models/cardModel'
+import { columnModel } from '~/models/columnModel'
 const createNew = async (reqBody) => {
   try {
-    const newCard = {
-      ...reqBody,
-    }
-    return await cardModel.createNew(newCard)
+    const newCard = await cardModel.createNew(reqBody)
+    // 2. Update cardOrderIds trong Column
+    await columnModel.Column.findByIdAndUpdate(
+      '68b4d307eacf1c87578e3369',
+      { $push: { cardOrderIds: newCard._id } }, // thêm vào cuối
+      { new: true }
+    )
+    return newCard
   } catch (error) {
     throw new Error('Error creating card: ' + error.message)
   }
--- src/services/columnService.js
+++ src/services/columnService.js
@@ -1,11 +1,15 @@
 import { columnModel } from '~/models/columnModel'
-
+import { boardModel } from '~/models/boardModel'
 const createNew = async (reqBody) => {
   try {
-    const newColumn = {
-      ...reqBody,
-    }
-    return await columnModel.createNew(newColumn)
+    const newColumn = await columnModel.createNew(reqBody)
+    // 2. Update columnOrderIds trong Board
+    await boardModel.Board.findByIdAndUpdate(
+      '68b408360c2a2b98e09d4265',
+      { $push: { columnOrderIds: newColumn._id } }, // thêm vào cuối
+      { new: true }
+    )
+    return newColumn
   } catch (error) {
     throw new Error('Error creating column: ' + error.message)
   }
